7b04b3d5aa9fedf8cb8eb23f46bc3474e8000f20
Ludovic <ludoverfaille@outlook.com>
2018-11-06 17:42:27 +0100

    Push classes Personnage/Arme/Zombie

---
+++ include/Arme.h
@@ -0,0 +1,28 @@
+#ifndef ARME_H
+#define ARME_H
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+class Arme
+{
+    public:
+        Arme(int puissance, int munition, string sprite);
+        Arme(const Arme& other);
+        bool operator==(const Arme& other);
+        virtual ~Arme();
+        Arme();
+
+        string str()const;
+
+    protected:
+
+    private:
+        int puissance;
+        int munition;
+        string sprite;
+};
+
+#endif // ARME_H
---
+++ src/Arme.cpp
@@ -0,0 +1,39 @@
+#include "Arme.h"
+
+Arme::Arme(int puissance, int munition, string sprite)
+{
+    this->puissance = puissance;
+    this->munition = munition;
+    this->sprite = sprite;
+}
+
+Arme::Arme()
+{
+    this->puissance = 100;
+    this->munition = 50;
+    this->sprite = "sprite.png";
+}
+
+Arme::Arme(const Arme& other){
+    this->puissance = other.puissance;
+    this->munition = other.munition;
+    this->sprite = other.sprite;
+}
+
+bool Arme::operator==(const Arme& other){
+    if(this->puissance == other.puissance && this->munition == other.munition && this->sprite == other.sprite){
+        return true;
+    }
+    return false;
+}
+
+Arme::~Arme()
+{
+    //dtor
+}
+
+string Arme::str()const{
+    stringstream strs;
+    strs<<puissance<<" "<<munition;
+    return strs.str();
+}
