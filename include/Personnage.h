28ea4503d8c44019bb21063fa3ac35715d6911e5
Ludovic <ludoverfaille@outlook.com>
2018-11-06 17:51:04 +0100

    Test classe Personnage

---
+++ include/Personnage.h
@@ -0,0 +1,28 @@
+#ifndef PERSONNAGE_H
+#define PERSONNAGE_H
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "Arme.h"
+
+class Personnage
+{
+    public:
+        Personnage(int vie, int vitesse,Arme arme);
+        Personnage(const Personnage& other);
+        bool operator==(const Personnage& other);
+        virtual ~Personnage();
+
+        string str()const;
+
+    protected:
+
+    private:
+        int vie;
+        int vitesse;
+        Arme arme;
+
+        string sprite;
+};
+
+#endif // PERSONNAGE_H
