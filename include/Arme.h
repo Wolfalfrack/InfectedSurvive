5e0c361bea359bb453eed7408ebb5c13a528f038
Ludovic <ludoverfaille@outlook.com>
2018-11-07 10:24:01 +0100

    Re Arme

--- include/Arme.h
+++ include/Arme.h
@@ -1,28 +1,16 @@
 #ifndef ARME_H
 #define ARME_H
-#include <string>
-#include <sstream>
-#include <iostream>
 
-using namespace std;
 
 class Arme
 {
     public:
-        Arme(int puissance, int munition, string sprite);
-        Arme(const Arme& other);
-        bool operator==(const Arme& other);
-        virtual ~Arme();
         Arme();
-
-        string str()const;
+        virtual ~Arme();
 
     protected:
 
     private:
-        int puissance;
-        int munition;
-        string sprite;
 };
 
 #endif // ARME_H
