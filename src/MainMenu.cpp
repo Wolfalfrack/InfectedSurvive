3f75ddcdfd1ea14017baa3270a47b9bcdd234440
Wolfalfrack <nicolas.verhaeghe14@gmail.com>
2018-11-13 11:32:32 +0100

    Deplacement

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -13,7 +13,7 @@ MainMenu::~MainMenu()
 
 void MainMenu::draw()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "InfectedSurvive");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
@@ -25,9 +25,26 @@ void MainMenu::draw()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+    {
+        shape.move(0,-1);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    {
+        shape.move(0,1);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+    {
+        shape.move(-1,0);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+    {
+        shape.move(1,0);
+    }
 
         window.clear();
         window.draw(shape);
         window.display();
     }
+
 }
