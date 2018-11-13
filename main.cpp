6669a191001f343ba3f47703095fe1d42731357b
Wolfalfrack <nicolas.verhaeghe14@gmail.com>
2018-11-13 11:15:39 +0100

    Modification main

--- main.cpp
+++ main.cpp
@@ -1,34 +1,16 @@
 #include <stdexcept>
 #include <iostream>
 #include "Application.h"
+
 using namespace std;
 
 int main()
 {
-    /*sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;*/
-
     try
     {
         Application app;
         app.run();
+
     }catch (std::exception& e)
 	{
 		std::cout << "\nEXCEPTION: " << e.what() << std::endl;
